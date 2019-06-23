72de24aa98288cf8d9bf9971d64f0a23d0289101
Jacob Allen <boat.mon@gmail.com>
2019-06-23 01:29:27 -0400

    pinponit the center on Miami, FL

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,15 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import ReactMapGL from "react-map-gl"
 
 export  default function App() {
-const []
+const [viewport, setViewport] = useState({
+  latitude:25.774266,
+  longitud:-80.193659,
+  width: '100ve',
+  height: '100vh',
+  zoom: 10
+});
 
-  return <div>
-      <ReactMapGL>
+  return (
+    <div>
+      <ReactMapGL {...viewport}>
         Markers here
       </ReactMapGL>
-    </div>;
+    </div>
+  );
 }
-
-export default App;
